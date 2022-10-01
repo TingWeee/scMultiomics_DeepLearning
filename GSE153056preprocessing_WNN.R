d45dec7a3454079c356fe1db1b65cbbc1c0eff8b
DarrenT <dtwj1998@gmail.com>
2022-10-01 19:37:51 +0800

    Fixed GSE153056 CITE-seq di-omic data

--- GSE153056preprocessing_WNN.R
+++ GSE153056preprocessing_WNN.R
@@ -1,64 +1,32 @@
+library(Seurat)
+library(dplyr)
+library(readr)
+adt = read.csv(file = 'GSM4633606_CITE_ADT_counts.tsv.gz', sep = '\t', header = TRUE, row.names = 1)
+rna = read.csv(file = 'GSM4633605_CITE_cDNA_counts.tsv.gz', sep = '\t', header = TRUE, row.names = 1)
 
-InstallData("thp1.eccite")
+head(rna)
 
-data("thp1.eccite")
+thp1 = CreateSeuratObject(counts = rna)
+Assays(thp1)
+adt_assay <- CreateAssayObject(counts = adt)
+thp1[["ADT"]] <- adt_assay
 
-citation('thp1.eccite.SeuratData')
 
-ce =  LoadData(ds = "thp1.eccite")
-ce
+rownames(thp1[["ADT"]])
+
 
 # RNA preprocessing
-DefaultAssay(ce) <- 'RNA' #ce has 3 assays 'RNA' and 'ADT' and 'HTO' and 'GDO'
+DefaultAssay(thp1) <- 'RNA' #ce has 3 assays 'RNA' and 'ADT' and 'HTO' and 'GDO'
 
-library(dplyr)
-ce<- NormalizeData(ce) %>% FindVariableFeatures() %>% ScaleData() %>% RunPCA()
+thp1<- NormalizeData(thp1) %>% FindVariableFeatures() %>% ScaleData()
 
+DefaultAssay(thp1) <- 'ADT'
+thp1<- NormalizeData(thp1) %>% FindVariableFeatures() %>% ScaleData()
 
-# ADT preprocessing 
-DefaultAssay(ce) <- 'ADT'
-# we will use all ADT features for dimensional reduction 
-VariableFeatures(ce) <- rownames(ce[["ADT"]])
-ce <- NormalizeData(ce, normalization.method = 'CLR', margin = 2) 
-ce<- ScaleData(ce) # Scale and center features in data 
-# we set a dimensional reduction name to avoid overwriting
-ce <- RunPCA(ce, reduction.name = 'apca')
+rna.scale <- GetAssayData(thp1[["RNA"]], slot="scale.data")[VariableFeatures(thp1[["RNA"]]), ]
+protein.scale <- GetAssayData(thp1[["ADT"]], slot="scale.data")
 
-# HTO preprocessing 
-DefaultAssay(ce) <- 'HTO'
-# we will use all ADT features for dimensional reduction 
-VariableFeatures(ce) <- rownames(ce[["HTO"]])
-ce <- NormalizeData(ce, normalization.method = 'CLR', margin = 2) 
-ce<- ScaleData(ce) # Scale and center features in data 
-# we set a dimensional reduction name to avoid overwriting
-ce <- RunPCA(ce, reduction.name = 'hpca')
-
-# GDO preprocessing 
-DefaultAssay(ce) <- 'GDO'
-# we will use all ADT features for dimensional reduction 
-VariableFeatures(ce) <- rownames(ce[["GDO"]])
-ce <- NormalizeData(ce, normalization.method = 'CLR', margin = 2) 
-ce<- ScaleData(ce) # Scale and center features in data 
-# we set a dimensional reduction name to avoid overwriting
-ce <- RunPCA(ce, reduction.name = 'gpca')
-
-ce <- FindMultiModalNeighbors(
-  object= ce , reduction.list = list("pca", "apca", "hpca", "gpca"), 
-  dims.list = list(1:30, 1:3, 1:11, 1:30))
-
-
-rna.scale <- GetAssayData(ce[["RNA"]], slot="scale.data")[VariableFeatures(ce[["RNA"]]), ]
-protein.scale <- GetAssayData(ce[["ADT"]], slot="scale.data")
-hto.scale <- GetAssayData(ce[["HTO"]], slot="scale.data")
-gdo.scale <- GetAssayData(ce[["GDO"]], slot="scale.data")
-
-metadata <- ce@meta.data # in this case data is already annotated
-
-dirr = 'D:/NUS-SPS github/scMultiomics_DeepLearning/data/thp1/'
+dirr = ''
 write.csv(rna.scale, gzfile(paste(dirr,"rna_scale.csv.gz", sep = '')))
 write.csv(protein.scale, gzfile(paste(dirr,"protein_scale.csv.gz", sep = '')))
-write.csv(metadata, gzfile(paste(dirr,"metadata.csv.gz", sep = '')))
-
-write.csv(hto.scale, gzfile(paste(dirr,"hto_scale.csv.gz", sep = '')))
-write.csv(gdo.scale, gzfile(paste(dirr,"gdo_scale.csv.gz", sep = '')))
 
